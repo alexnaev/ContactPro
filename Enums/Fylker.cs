57794c3c2a63bcf21589472801367268c99b8fb7
alexnaev <alexandernaev@protonmail.com>
2024-02-09 14:27:21 +0100

    Added App Models

---
+++ Enums/Fylker.cs
@@ -0,0 +1,21 @@
+namespace ContactPro.Enums
+{
+    public enum Fylker
+    {
+        Agder,
+        Akershus,
+        Buskerud,
+        Finnmark,
+        Innlandet,
+        MøreOgRomsdal,
+        Nordland,
+        Oslo,
+        Rogaland,
+        Telemark,
+        Troms,
+        Trøndelag,
+        Vestfold,
+        Vestland,
+        Østfold
+    }
+}
