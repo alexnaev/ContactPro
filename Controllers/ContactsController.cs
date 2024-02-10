e68f672747470be3c5f05c097fcce317a7275cf9
alexnaev <alexandernaev@protonmail.com>
2024-02-10 19:43:13 +0100

    Adding States

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -177,12 +177,20 @@ namespace ContactPro
                 return NotFound();
             }
 
-            var contact = await _context.Contacts.FindAsync(id);
+            string appUserId = _userManager.GetUserId(User);
+
+            // var contact = await _context.Contacts.FindAsync(id);
+            var contact = await _context.Contacts.Where(c => c.Id == id && c.AppUserId == appUserId)
+                                                 .FirstOrDefaultAsync();
+            
             if (contact == null)
             {
                 return NotFound();
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", contact.AppUserId);
+            
+            ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(Fylker)).Cast<Fylker>().ToList());
+            ViewData["CategoryList"] = new MultiSelectList(await _addressBookService.GetUserCategoriesAsync(appUserId), "Id", "Name");
+            
             return View(contact);
         }
 
